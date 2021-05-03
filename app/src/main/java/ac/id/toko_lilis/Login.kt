22b130cc95752a01e6fdfda385ef62158bebe7db
lilissonia <lilissonia31@gmail.com>
2021-05-03 16:03:01 +0700

    lilissonia

---
+++ app/src/main/java/ac/id/toko_lilis/Login.kt
@@ -0,0 +1,33 @@
+package ac.id.toko_lilis
+
+import ac.id.toko_lilis.R
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
+import ac.id.toko_lilis.Register
+import kotlinx.android.synthetic.main.activity_login.*
+
+class Login : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        lateinit var toolbar: Toolbar
+        toolbar = findViewById(R.id.toolbar_login)
+        setSupportActionBar(toolbar)
+        supportActionBar?.apply {
+            setDisplayHomeAsUpEnabled(true)
+            setDisplayShowHomeEnabled(true)
+        }
+
+        btn_register_register.setOnClickListener {
+            startActivity(Intent(this, Register::class.java))
+            finish()
+        }
+    }
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressed()
+        return super.onSupportNavigateUp()
+    }
+}
